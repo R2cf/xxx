dc62463ddde0d2cc00def29ac5738aace6f3d6b2
ojha111 <152144787+ojha111@users.noreply.github.com>
2024-04-27 23:58:13 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 29403305
 api_hash = "54c435573d0f2d9dada270a0cb493b36"
-bot_token = "7174084876:AAGOiyCsQLxVbwpOoVIPxwMJmwNi2bP9-Ok"
+bot_token = "6500615538:AAGCsJp1zf2EclVNMnbXWVlc1UOsqpmorm8"
 
