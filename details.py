6d66d4b48f4244a2b401b97c22f93289f3329a35
Vijay sahu <88523094+R2cf@users.noreply.github.com>
2024-06-15 06:57:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 22626671
 api_hash = "4081196bbe59a5d6a7fd786b51230bd9"
-bot_token = "7419027236:AAFVfT93pmP3XCkVx3r_wlFncv-aYTMBt4s"
+bot_token = "7061081267:AAH8frAWfGkf0GhU1DZNcW04N6bmzJc5k8k"
 
