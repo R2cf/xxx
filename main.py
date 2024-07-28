b8660e4f60bcdf7dc3c3a782d0d5e5c003542b92
Vijay sahu <88523094+R2cf@users.noreply.github.com>
2024-07-28 15:30:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,7 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE
-
-
+from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
 import requests
 import json
 import subprocess
